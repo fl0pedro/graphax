7286f44fd80c05e9ce17302b0d0e4df357fc3f7d
Florain Assmuth <florianassmuth@gmail.com>
2025-10-03 12:58:59 +0200

    decrease range 28->19

--- performance_measurements/bst_perf_test.py
+++ performance_measurements/bst_perf_test.py
@@ -220,7 +220,7 @@ def test(size, k1, k2, stx_dims, sty_dims):
     print("")
     return res
 
-range_ = [(i%9+1)*10**(i//9) for i in range(28)]
+range_ = [(i%9+1)*10**(i//9) for i in range(19)]
 res = {}
 
 for i, (block_nums, block_size) in enumerate(product(range_, range_)):
