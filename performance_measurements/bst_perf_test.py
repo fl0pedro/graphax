863ab8c2f02966f75975d5073aa642439034bb3b
Florain Assmuth <florianassmuth@gmail.com>
2025-10-03 04:59:34 +0200

    estimate only

--- performance_measurements/bst_perf_test.py
+++ performance_measurements/bst_perf_test.py
@@ -167,14 +167,14 @@ def test(size, k1, k2, stx_dims, sty_dims):
 
     res["sparse"] = dict()
     res["sparse"]["estimate"] = jit_matmul.lower(stx, sty).cost_analysis()
-    if res["sparse"]["estimate"] is not None \
-            and res["sparse"]["estimate"]["bytes accessed"] <= MAX_MEMORY:
-        res["sparse"]["measured"] = []
-        for _ in range(20):
-            _, r = profile_jax(jit_matmul, stx, sty, poll_ms=1)
-            res["sparse"]["measured"].append(r)
-    else:
-        return res
+    #if res["sparse"]["estimate"] is not None \
+    #        and res["sparse"]["estimate"]["bytes accessed"] <= MAX_MEMORY:
+    #    res["sparse"]["measured"] = []
+    #    for _ in range(20):
+    #        _, r = profile_jax(jit_matmul, stx, sty, poll_ms=1)
+    #        res["sparse"]["measured"].append(r)
+    #else:
+    #    return res
 
     if get_dense_expansion_bytes(stx, sty) <= MAX_MEMORY:
         x = stx.dense()
@@ -185,12 +185,12 @@ def test(size, k1, k2, stx_dims, sty_dims):
     res["dense"] = dict()
     dnums = ((tuple(d.id for d in stx.primal_dims), tuple(d.id for d in sty.out_dims)), ((), ()))
     res["dense"]["estimate"] = jit_dot.lower(x, y, dimension_numbers=dnums).cost_analysis()
-    if res["dense"]["estimate"] is not None \
-            and res["dense"]["estimate"]["bytes accessed"] <= MAX_MEMORY:
-        res["dense"]["measured"] = []
-        for _ in range(20):
-            _, r = profile_jax(jit_dot, x, y, dimension_numbers=dnums, poll_ms=1)
-            res["dense"].append(r)
+    #if res["dense"]["estimate"] is not None \
+    #        and res["dense"]["estimate"]["bytes accessed"] <= MAX_MEMORY:
+    #    res["dense"]["measured"] = []
+    #    for _ in range(20):
+    #        _, r = profile_jax(jit_dot, x, y, dimension_numbers=dnums, poll_ms=1)
+    #        res["dense"].append(r)
     
     return res
 
