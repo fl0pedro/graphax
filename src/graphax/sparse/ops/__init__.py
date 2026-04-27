a8b110db388e36adf141645a3da32719821f4b83
Florian Assmuth <florianassmuth@gmail.com>
2026-04-27 18:51:02 +0200

    refactor: optimize sparse tensor operations and remapping

--- src/graphax/sparse/ops/__init__.py
+++ src/graphax/sparse/ops/__init__.py
@@ -1,4 +1,4 @@
 from .dense import dense
 from .matmul import matmul
-from .elementwise import elementwise
+from .elementwise import elementwise, add_w_counts, mul_w_counts
 from .transpose import transpose
